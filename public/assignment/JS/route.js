ac2bd3b1347d047f9a044d4ea4aeba9669eb859b
Maelstroms <workpartystudy@yahoo.com>
2016-05-24 14:35:24 -0400

    added rounting in route.jsto get it to work

--- public/assignment/JS/route.js
+++ public/assignment/JS/route.js
@@ -16,9 +16,37 @@
             })
             .when("/register", {
                 templateURL:  "/views/User/register.view.client.html"
+            })
+            .when("/User/:uid/website", {
+                templateURL:  "/views/Website/website-list.view.client.html"
+            })
+            .when("/User/:uid/website/new", {
+                templateURL:  "/views/Website/website-new.view.client.html"
+            })
+            .when("/User/:uid/website/:wid", {
+                templateURL:  "/views/Website/website-edit.view.client.html"
+            })
+            .when("/User/:uid/website/:wid/page", {
+                templateURL:  "/views/Page/page-list.view.client.html"
+            })
+            .when("/User/:uid/website/:wid/page/new", {
+                templateURL:  "/views/Page/page-new.view.client.html"
+            })
+            .when("/User/:uid/website/:wid/page/:pid", {
+                templateURL:  "/views/Page/page-edit.view.client.html"
+            })
+            .when("/User/:uid/website/:wid/page/:pid/widget", {
+                templateURL:  "/views/W idgetage/widget-list.view.client.html"
+            })
+            .when("/User/:uid/website/:wid/page/:pid/widget/new", {
+                templateURL:  "/views/Widget/widget-choose.view.client.html"
+            })
+            .when("/User/:uid/website/:wid/page/:pid/widget/:wgid", {
+                templateURL:  "/views/Widget/widget-edit.view.client.html"
             });
 
-    }
+            }
+    
 
 })();
 
