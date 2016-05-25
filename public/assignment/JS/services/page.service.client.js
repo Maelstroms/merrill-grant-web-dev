37ba066872145fd0e10a8552c618af9d23ed2ecf
Maelstroms <workpartystudy@yahoo.com>
2016-05-25 18:12:56 -0400

    page service js boiler plate added

--- public/assignment/JS/services/page.service.client.js
+++ public/assignment/JS/services/page.service.client.js
@@ -2,3 +2,28 @@
  * Created by grantmerrill on 5/24/16.
  */
 
+(function() {
+    angular.module("WebAppMaker")
+        .factory("PageService", PageService);
+    var pages = [
+        { "_id": "321", "name": "Post 1", "websiteId": "456" },
+        { "_id": "432", "name": "Post 2", "websiteId": "456" },
+        { "_id": "543", "name": "Post 3", "websiteId": "456" }
+    ];
+
+    function PageService(){
+        var api = {
+            createPage : createPage,
+            findPageByWebsiteId: findPageByWebsiteId,
+            findPageById: findPageById,
+            updatePage: updatePage,
+            deletePage: deletePage
+        };
+
+        function createPage(websiteId, page){}
+        function findPageByWebsiteId(websiteId){}
+        function findPageById(pageId){}
+        function updatePage(pageId, page){}
+        function deletePage(pageId){}
+    }
+})();
