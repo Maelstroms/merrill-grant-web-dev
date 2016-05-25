779f1419a6e67964c64c1c3ed65408d70e93574f
Maelstroms <workpartystudy@yahoo.com>
2016-05-25 18:17:56 -0400

    widget service js boiler plate added

--- public/assignment/JS/services/widget.service.client.js
+++ public/assignment/JS/services/widget.service.client.js
@@ -1,3 +1,29 @@
 /**
  * Created by grantmerrill on 5/25/16.
  */
+
+(function() {
+    angular.module("WebAppMaker")
+        .factory("WidgetService", WidgetService);
+    var pages = [
+        { "_id": "321", "name": "Post 1", "websiteId": "456" },
+        { "_id": "432", "name": "Post 2", "websiteId": "456" },
+        { "_id": "543", "name": "Post 3", "websiteId": "456" }
+    ];
+
+    function WidgetService(){
+        var api = {
+            createWidget : createWidget,
+            findWidgetsByPageId: findWidgetsByPageId,
+            findWidgetById: findWidgetById,
+            updateWidget: updateWidget,
+            deleteWidget: deleteWidget
+        };
+
+        function createWidget(pageId, widget){}
+        function findWidgetsByPageId(pageId){}
+        function findWidgetById(widgetId){}
+        function updateWidget(widgetId, widget){}
+        function deleteWidget(widgetId){}
+    }
+})();
