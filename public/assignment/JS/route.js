547b686449bfe39cb6d29335fabb2f07cc2e44d9
Maelstroms <workpartystudy@yahoo.com>
2016-05-26 15:27:23 -0400

    bound controllers to routes, create widget controllers

--- public/assignment/JS/route.js
+++ public/assignment/JS/route.js
@@ -7,46 +7,73 @@
         $routeProvider
             .when("default", {
                 templateUrl:  "/assignment/views/User/login.view.client.html",
-                controller: "LoginController"
+                controller: "LoginController",
+                controllerAs: "model"
             })
             .when("/login", {
-                templateUrl: "/assignment/views/User/login.view.client.html"
+                templateUrl: "/assignment/views/User/login.view.client.html",
+                controller: "LoginController",
+                controllerAs: "model"
             })
             .when("/", {
-                templateUrl:  "/assignment/views/User/login.view.client.html"
+                templateUrl:  "/assignment/views/User/login.view.client.html",
+                controller: "LoginController",
+                controllerAs: "model"
             })
             .when("/register", {
-                templateUrl:  "/assignment/views/User/register.view.client.html"
+                templateUrl:  "/assignment/views/User/register.view.client.html",
+                controller: "RegisterController",
+                controllerAs: "model"
             })
             .when("/User/:uid", {
-                templateUrl: "/assignment/views/User/profile.view.client.html"
+                templateUrl: "/assignment/views/User/profile.view.client.html",
+                controller: "ProfileController",
+                controllerAs: "model"
             })
             .when("/User/:uid/website", {
-                templateUrl:  "/assignment/views/Website/website-list.view.client.html"
+                templateUrl:  "/assignment/views/Website/website-list.view.client.html",
+                controller: "WebsiteListController",
+                controllerAs: "model"
             })
             .when("/User/:uid/website/new", {
-                templateUrl:  "/assignment/views/Website/website-new.view.client.html"
+                templateUrl:  "/assignment/views/Website/website-new.view.client.html",
+                controller: "WebsiteNewController",
+                controllerAs: "model"
             })
             .when("/User/:uid/website/:wid", {
-                templateUrl:  "/assignment/views/Website/website-edit.view.client.html"
+                templateUrl:  "/assignment/views/Website/website-edit.view.client.html",
+                controller: "WebsiteEditController",
+                controllerAs: "model"
             })
             .when("/User/:uid/website/:wid/page", {
-                templateUrl:  "/assignment/views/Page/page-list.view.client.html"
+                templateUrl:  "/assignment/views/Page/page-list.view.client.html",
+                controller: "PageListController",
+                controllerAs: "model"
             })
             .when("/User/:uid/website/:wid/page/new", {
-                templateUrl:  "/assignment/views/Page/page-new.view.client.html"
+                templateUrl:  "/assignment/views/Page/page-new.view.client.html",
+                controller: "PageNewController",
+                controllerAs: "model"
             })
             .when("/User/:uid/website/:wid/page/:pid", {
-                templateUrl:  "/assignment/views/Page/page-edit.view.client.html"
+                templateUrl:  "/assignment/views/Page/page-edit.view.client.html",
+                controller: "PageEditController",
+                controllerAs: "model"
             })
             .when("/User/:uid/website/:wid/page/:pid/widget", {
-                templateUrl:  "/assignment/views/W idgetage/widget-list.view.client.html"
+                templateUrl:  "/assignment/views/Widgetage/widget-list.view.client.html",
+                controller: "WidgetListController",
+                controllerAs: "model"
             })
             .when("/User/:uid/website/:wid/page/:pid/widget/new", {
-                templateUrl:  "/assignment/views/Widget/widget-choose.view.client.html"
+                templateUrl:  "/assignment/views/Widget/widget-choose.view.client.html",
+                controller: "WidgetNewController",
+                controllerAs: "model"
             })
             .when("/User/:uid/website/:wid/page/:pid/widget/:wgid", {
-                templateUrl:  "/assignment/views/Widget/widget-edit.view.client.html"
+                templateUrl:  "/assignment/views/Widget/widget-edit.view.client.html",
+                controller: "WidgetEditController",
+                controllerAs: "model"
             });
 
             }
