29a5d7f07846bfce06de3b4eb471500bc3a4e913
Maelstroms <workpartystudy@yahoo.com>
2016-05-26 16:20:29 -0400

    website controller boilerplate

--- public/assignment/views/Website/website-list.controller.client.js
+++ public/assignment/views/Website/website-list.controller.client.js
@@ -1,3 +1,17 @@
-/**
+(function(){
+    angular
+        .module("WebAppMaker")
+        .controller("WebsiteListController", WebsiteListController);
+
+    function WebsiteListController($routeParams, WebsiteService) {
+        var vm = this;
+
+        function init() {
+            var userId = $routeParams.userId;
+            vm.websites = WebsiteService.findWebsitesForUser(userId);
+        }
+        init();
+    }
+})();/**
  * Created by grantmerrill on 5/26/16.
  */
