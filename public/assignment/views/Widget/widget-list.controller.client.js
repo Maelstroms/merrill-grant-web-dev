664394c2e74d4039e5b876debef9ff7302b420c0
Maelstroms <workpartystudy@yahoo.com>
2016-05-29 12:01:15 -0400

    got youtube display to work

--- public/assignment/views/Widget/widget-list.controller.client.js
+++ public/assignment/views/Widget/widget-list.controller.client.js
@@ -2,48 +2,30 @@
     angular
         .module("WebAppMaker")
         .controller("WidgetListController", WidgetListController)
-        .controller("HeaderWidgetController", HeaderWidgetController)
-        .controller("ImageWidgetController", ImageWidgetController)
-        .controller("YoutubeWidgetController", YoutubeWidgetController);
 
-    function WidgetListController($routeParams, WidgetService) {
+    function WidgetListController($sce, $routeParams, WidgetService) {
         var vm = this;
         vm.route = $routeParams;
+        vm.getSafeHtml = getSafeHtml;
+        vm.getSafeUrl = getSafeUrl;
 
         function init() {
             var pid = $routeParams.pid;
             vm.widgets = WidgetService.findWidgetsByPageId(pid);
         }
         init();
-    }
-    function HeaderWidgetController($routeParams, WidgetService){
-        var vm = this;
-        vm.route = $routeParams;
-        var widget;
-        vm.dataholder ={};
-        function init() {
-            console.log($routeParams);
-            var wgid = $routeParams["wgid"];
-            console.log(widget);
-            vm.header = WidgetService.findWidgetById();
+
+        function getSafeHtml(widget) {
+            return $sce.trustAsHtml(widget.text);
         }
-        init();
-    }
-    function ImageWidgetController($routeParams, WidgetService){
-        var vm = this;
-        vm.route = $routeParams;
-        function init() {
+
+        function getSafeUrl(widget) {
+            var urlParts = widget.url.split("/");
+            var id = urlParts[urlParts.length - 1];
+            var url = "https://www.youtube.com/embed/" + id;
+            return $sce.trustAsResourceUrl(url);
 
         }
-        init();
-
     }
-    function YoutubeWidgetController($routeParams, WidgetService){
-        var vm = this;
-        vm.route = $routeParams;
-        function init() {
 
-        }
-        init();
-    }
 })();
